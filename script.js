43a08b3165cc4b0e75d9da8f9a0bac688ff5231a
Mosawer Qayomi <student199262@worcesterschools.net>
2023-11-28 17:05:58 -0500

    first

---
+++ script.js
@@ -0,0 +1,71 @@
+console.log("hHello world");
+
+// Weird scope - Don't use
+// var x = 5;
+// Regular old varibale
+let y = 6;
+y=10; // you can change the value 
+
+const z = 7;// This is a contant. Can't be change
+// z=10; // this throws and error.
+
+console.log(y,z)
+console.log(y+z)
+console.log(y-z)
+console.log(y*z)
+console.log(y/z)
+console.log(y%z)// This does division but returns the remainder
+console.log(y**z)// exponets
+
+let fnsme="Mosawer"
+let lname="Qayomi"
+console.log(fname + "   " + lname);
+
+console.log(`${ fname}  ${lname}`);
+console.log(`${ fname}  ${ 5*9 } `);
+console.log(lname + 6)
+
+
+let user ="ronaldo";
+let pas ="messi";
+
+if (user == "ronaldo") {
+    console.log('you are ' + user);
+}
+ 
+user = "ronaldo";
+if (USER == "ronaldo") {
+    console.log('you are ' + );
+} else {
+    console.error("denied")
+}
+
+user = "ronaldo";
+if (user == "ronaldo" && pass== "messi") {
+    console.log('you are ' + user + ' and your pass is ' + pass);
+} else {
+    console.error("denied")
+}
+
+const licence = 18;
+const jrop = 16;
+const rental = 25
+
+let myAge = 15;
+
+if (mabey < licence && myAge >=jrop) {
+    console.log('you can have a jrop license');
+}
+
+if (myAge >= licence) { 
+    console.log('you can have a regular license');
+}
+
+if (myAge >= rental) {
+    console.log('you can rent a car');
+}
+
+if (myAge < jrop) {
+    console.log("you're to young to drive")
+}
+
